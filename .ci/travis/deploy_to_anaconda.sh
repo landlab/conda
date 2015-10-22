868b03ebd0a10bf841db4edceaf10b6258d00836
mcflugen <mcflugen@gmail.com>
2015-10-21 23:35:16 -0600

    Print some status messages.

--- .ci/travis/deploy_to_anaconda.sh
+++ .ci/travis/deploy_to_anaconda.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+echo Deploying $CONDA_PACKAGE_FILE to $CONDA_USERNAME
+echo Login...
 anaconda login --username=$CONDA_USERNAME --password=$CONDA_PASSWORD
+echo Upload...
 anaconda upload --user landlab --channel main $CONDA_PACKAGE_FILE
